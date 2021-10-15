34d664eb2fb79ea655230bfbfd23cd5ef115c494
ScottSut <xxw@ScottSutdeMacBook-Pro.local>
2021-10-15 19:22:17 +0800

    feat: add notfound page

---
+++ frontend/src/app/pages/NotFoundPage/__tests__/index.test.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { HelmetProvider } from 'react-helmet-async';
+import { MemoryRouter } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import { NotFoundPage } from '..';
+
+const renderPage = () =>
+  renderer.create(
+    <MemoryRouter>
+      <HelmetProvider>
+        <NotFoundPage />
+      </HelmetProvider>
+    </MemoryRouter>,
+  );
+
+describe('<NotFoundPage />', () => {
+  it('should match snapshot', () => {
+    const notFoundPage = renderPage();
+    expect(notFoundPage.toJSON()).toMatchSnapshot();
+  });
+
+  it('should should contain Link', () => {});
+});
