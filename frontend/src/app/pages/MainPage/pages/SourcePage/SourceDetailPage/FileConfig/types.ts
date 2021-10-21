8f6e50a5ddc7c2cb6edab87cec1eaeeb1413d64e
ScottSut <xxw@ScottSutdeMacBook-Pro.local>
2021-10-21 15:17:56 +0800

    fix(Source): file source type edit bug

--- frontend/src/app/pages/MainPage/pages/SourcePage/SourceDetailPage/FileConfig/types.ts
+++
@@ -1,8 +0,0 @@
-import { Schema } from 'app/pages/MainPage/pages/ViewPage/slice/types';
-
-export interface FileSourceConfig {
-  tableName: string;
-  format: string;
-  path: string;
-  columns: Schema[];
-}
