8ad373b70bbc423f2b8a9927530410133b1d647d
761302945@qq.com <761302945@qq.com>
2021-10-25 17:56:23 +0800

    feat: add jdbc adapter for impala

---
+++ data-providers/jdbc-data-provider/src/main/java/datart/data/provider/jdbc/adapters/ImpalaDataProviderAdapter.java
@@ -0,0 +1,44 @@
+/*
+ * Datart
+ * <p>
+ * Copyright 2021
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package datart.data.provider.jdbc.adapters;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ImpalaDataProviderAdapter extends JdbcDataProviderAdapter {
+
+    @Override
+    public Set<String> readAllTables(String database) throws SQLException {
+        try (Connection conn = getConn()) {
+            Set<String> tables = new HashSet<>();
+            DatabaseMetaData metadata = conn.getMetaData();
+            try (ResultSet rs = metadata.getTables(null, null, "%", new String[]{"TABLE", "VIEW"})) {
+                while (rs.next()) {
+                    String tableName = rs.getString(3);
+                    tables.add(tableName);
+                }
+            }
+            return tables;
+        }
+    }
+}
