b831f69810998205e70c59e0cab0da19afec485c
Stephen Cui <229063661@qq.com>
2021-11-25 12:34:27 +0800

    feat(chart): should cache chart config when switch chart in chart editor

--- frontend/src/app/pages/ChartWorkbenchPage/components/ChartOperationPanel/components/ChartGraphPanel.tsx
+++ frontend/src/app/pages/ChartWorkbenchPage/components/ChartOperationPanel/components/ChartGraphPanel.tsx
@@ -39,100 +39,95 @@ const ChartGraphPanel: FC<{
   chart?: Chart;
   chartConfig?: ChartConfig;
   onChartChange: (c: Chart) => void;
-}> = memo(
-  ({ chart, chartConfig, onChartChange }) => {
-    const t = useI18NPrefix(`viz.palette.graph`);
-    const chartManager = ChartManager.instance();
-    const [allCharts] = useState<Chart[]>(chartManager.getAllCharts());
-    const [requirementsStates, setRequirementStates] = useState<object>({});
+}> = memo(({ chart, chartConfig, onChartChange }) => {
+  const t = useI18NPrefix(`viz.palette.graph`);
+  const chartManager = ChartManager.instance();
+  const [allCharts] = useState<Chart[]>(chartManager.getAllCharts());
+  const [requirementsStates, setRequirementStates] = useState<object>({});
 
-    useEffect(() => {
-      const dict = allCharts?.reduce((acc, cur) => {
-        const transferedChartConfig = transferChartDataConfig(
-          CloneValueDeep(cur?.config),
-          chartConfig,
-        ) as ChartConfig;
-        const isMatch = cur?.isMatchRequirement(transferedChartConfig);
-        acc[cur.meta.id] = isMatch;
-        return acc;
-      }, {});
-      setRequirementStates(dict);
-    }, [allCharts, chartConfig]);
+  useEffect(() => {
+    const dict = allCharts?.reduce((acc, cur) => {
+      const transferedChartConfig = transferChartDataConfig(
+        CloneValueDeep(cur?.config),
+        chartConfig,
+      ) as ChartConfig;
+      const isMatch = cur?.isMatchRequirement(transferedChartConfig);
+      acc[cur.meta.id] = isMatch;
+      return acc;
+    }, {});
+    setRequirementStates(dict);
+  }, [allCharts, chartConfig]);
 
-    const handleChartChange = useCallback(
-      chartId => () => {
-        const chart = chartManager.getById(chartId);
-        if (!!chart) {
-          onChartChange(chart);
-        }
-      },
-      [chartManager, onChartChange],
-    );
+  const handleChartChange = useCallback(
+    chartId => () => {
+      const chart = chartManager.getById(chartId);
+      if (!!chart) {
+        onChartChange(chart);
+      }
+    },
+    [chartManager, onChartChange],
+  );
 
-    const renderChartRequirments = requirements => {
-      const lintMessages = requirements?.flatMap((requirement, index) => {
-        return [ChartDataSectionType.GROUP, ChartDataSectionType.AGGREGATE].map(
-          type => {
-            const limit = requirement[type.toLocaleLowerCase()];
-            const getMaxValueStr = limit =>
-              !!limit && +limit >= 999 ? 'N' : limit;
+  const renderChartRequirments = requirements => {
+    const lintMessages = requirements?.flatMap((requirement, index) => {
+      return [ChartDataSectionType.GROUP, ChartDataSectionType.AGGREGATE].map(
+        type => {
+          const limit = requirement[type.toLocaleLowerCase()];
+          const getMaxValueStr = limit =>
+            !!limit && +limit >= 999 ? 'N' : limit;
 
-            return (
-              <li key={type + index}>
-                {Number.isInteger(limit)
-                  ? t('onlyAllow', undefined, {
-                      type: t(type),
-                      num: getMaxValueStr(limit),
-                    })
-                  : Array.isArray(limit) && limit.length === 2
-                  ? t('allowRange', undefined, {
-                      type: t(type),
-                      start: limit?.[0],
-                      end: getMaxValueStr(limit?.[1]),
-                    })
-                  : null}
-              </li>
-            );
-          },
-        );
-      });
-      return <ul>{lintMessages}</ul>;
-    };
+          return (
+            <li key={type + index}>
+              {Number.isInteger(limit)
+                ? t('onlyAllow', undefined, {
+                    type: t(type),
+                    num: getMaxValueStr(limit),
+                  })
+                : Array.isArray(limit) && limit.length === 2
+                ? t('allowRange', undefined, {
+                    type: t(type),
+                    start: limit?.[0],
+                    end: getMaxValueStr(limit?.[1]),
+                  })
+                : null}
+            </li>
+          );
+        },
+      );
+    });
+    return <ul>{lintMessages}</ul>;
+  };
 
-    return (
-      <StyledChartGraphPanel>
-        {allCharts.map(c => (
-          <Tooltip
-            key={c?.meta?.id}
-            title={
-              <>
-                {c?.meta?.name}
-                {renderChartRequirments(c?.meta?.requirements)}
-              </>
-            }
-          >
-            <IconWrapper>
-              <StyledChartIcon
-                isMatchRequirement={!!requirementsStates?.[c?.meta?.id]}
-                fontSize={FONT_SIZE_ICON_MD}
-                size={SPACE_TIMES(9)}
-                className={classnames({
-                  active: c?.meta?.id === chart?.meta?.id,
-                })}
-                onClick={handleChartChange(c?.meta?.id)}
-              >
-                <i className={c?.meta?.icon} />
-              </StyledChartIcon>
-            </IconWrapper>
-          </Tooltip>
-        ))}
-      </StyledChartGraphPanel>
-    );
-  },
-  (prev, next) => {
-    return prev.chart === next.chart && prev.chartConfig === next.chartConfig;
-  },
-);
+  return (
+    <StyledChartGraphPanel>
+      {allCharts.map(c => (
+        <Tooltip
+          key={c?.meta?.id}
+          title={
+            <>
+              {c?.meta?.name}
+              {renderChartRequirments(c?.meta?.requirements)}
+            </>
+          }
+        >
+          <IconWrapper>
+            <StyledChartIcon
+              isMatchRequirement={!!requirementsStates?.[c?.meta?.id]}
+              fontSize={FONT_SIZE_ICON_MD}
+              size={SPACE_TIMES(9)}
+              className={classnames({
+                active: c?.meta?.id === chart?.meta?.id,
+              })}
+              onClick={handleChartChange(c?.meta?.id)}
+            >
+              <i className={c?.meta?.icon} />
+            </StyledChartIcon>
+          </IconWrapper>
+        </Tooltip>
+      ))}
+    </StyledChartGraphPanel>
+  );
+});
 
 export default ChartGraphPanel;
 
