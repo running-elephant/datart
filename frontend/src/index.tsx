87224565224fe82004d37fc5380c66e0721fe4ef
Stephen Cui <229063661@qq.com>
2021-12-24 14:05:32 +0800

    perf(tool): only enable performance log in development mode

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -17,46 +17,47 @@ import { ThemeProvider } from 'styles/theme/ThemeProvider';
 import { Debugger } from 'utils/debugger';
 import './locales/i18n';
 
-Debugger.instance.setEnable(true);
-export const store = configureAppStore();
 const MOUNT_NODE = document.getElementById('root') as HTMLElement;
-/**
- *  hot-key [control,shift,command,c]
- */
+const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
+const InspectorWrapper = IS_DEVELOPMENT ? Inspector : React.Fragment;
 
+Debugger.instance.setEnable(IS_DEVELOPMENT);
+export const store = configureAppStore();
 const MainApp = <App />;
 
-const InspectorWrapper =
-  process.env.NODE_ENV === 'development' ? Inspector : React.Fragment;
-ChartManager.instance()
-  .load()
-  .catch(err => console.error('Fail to load customize charts with ', err))
-  .finally(() => {
-    ReactDOM.render(
-      <InspectorWrapper>
-        <Provider store={store}>
-          <ThemeProvider>
-            <ConfigProvider locale={zh_CN}>
-              <HelmetProvider>
-                <React.StrictMode>{MainApp}</React.StrictMode>
-              </HelmetProvider>
-            </ConfigProvider>
-          </ThemeProvider>
-        </Provider>
-      </InspectorWrapper>,
-      MOUNT_NODE,
-    );
+Debugger.instance.measure('Main App', () => {
+  ChartManager.instance()
+    .load()
+    .catch(err => console.error('Fail to load customize charts with ', err))
+    .finally(() => {
+      ReactDOM.render(
+        <InspectorWrapper>
+          <Provider store={store}>
+            <ThemeProvider>
+              <ConfigProvider locale={zh_CN}>
+                <HelmetProvider>
+                  <React.StrictMode>{MainApp}</React.StrictMode>
+                </HelmetProvider>
+              </ConfigProvider>
+            </ThemeProvider>
+          </Provider>
+        </InspectorWrapper>,
+        MOUNT_NODE,
+      );
 
-    // Hot reloadable translation json files
-    if (module.hot) {
-      module.hot.accept(['./locales/i18n'], () => {
-        // No need to render the App again because i18next works with the hooks
-      });
-    }
-
-    if (process.env.NODE_ENV === 'production') {
-      if (typeof (window as any).__REACT_DEVTOOLS_GLOBAL_HOOK__ === 'object') {
-        (window as any).__REACT_DEVTOOLS_GLOBAL_HOOK__.inject = () => void 0;
+      // Hot reloadable translation json files
+      if (module.hot) {
+        module.hot.accept(['./locales/i18n'], () => {
+          // No need to render the App again because i18next works with the hooks
+        });
       }
-    }
-  });
+
+      if (!IS_DEVELOPMENT) {
+        if (
+          typeof (window as any).__REACT_DEVTOOLS_GLOBAL_HOOK__ === 'object'
+        ) {
+          (window as any).__REACT_DEVTOOLS_GLOBAL_HOOK__.inject = () => void 0;
+        }
+      }
+    });
+});
