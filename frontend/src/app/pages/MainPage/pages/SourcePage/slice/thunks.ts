4ea1f9d95304bb3e7f37cbf413c48caf94dd1241
scottsut <scottsut@163.com>
2021-12-24 12:16:06 +0800

    fix(Source): create button does not display when have create permission

--- frontend/src/app/pages/MainPage/pages/SourcePage/slice/thunks.ts
+++ frontend/src/app/pages/MainPage/pages/SourcePage/slice/thunks.ts
@@ -1,4 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import { selectOrgId } from 'app/pages/MainPage/slice/selectors';
+import { getLoggedInUserPermissions } from 'app/pages/MainPage/slice/thunks';
+import { RootState } from 'types';
 import { request } from 'utils/request';
 import { errorHandle } from 'utils/utils';
 import {
@@ -56,23 +59,29 @@ export const getSource = createAsyncThunk<Source, string>(
   },
 );
 
-export const addSource = createAsyncThunk<Source, AddSourceParams>(
-  'source/addSource',
-  async ({ source, resolve }) => {
-    try {
-      const { data } = await request<Source>({
-        url: '/sources',
-        method: 'POST',
-        data: source,
-      });
-      resolve(data.id);
-      return data;
-    } catch (error) {
-      errorHandle(error);
-      throw error;
-    }
-  },
-);
+export const addSource = createAsyncThunk<
+  Source,
+  AddSourceParams,
+  { state: RootState }
+>('source/addSource', async ({ source, resolve }, { getState, dispatch }) => {
+  try {
+    const { data } = await request<Source>({
+      url: '/sources',
+      method: 'POST',
+      data: source,
+    });
+
+    // FIXME 拥有Read权限等级的扁平结构资源新增后需要更新权限字典；后续如改造为目录结构则删除该逻辑
+    const orgId = selectOrgId(getState());
+    await dispatch(getLoggedInUserPermissions(orgId));
+
+    resolve(data.id);
+    return data;
+  } catch (error) {
+    errorHandle(error);
+    throw error;
+  }
+});
 
 export const editSource = createAsyncThunk<Source, EditSourceParams>(
   'source/editSource',
