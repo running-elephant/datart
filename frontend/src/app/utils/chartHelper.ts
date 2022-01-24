def14ad6856b227e96ccb3990128b6a89fad721d
Stephen Cui <229063661@qq.com>
2022-01-24 15:15:27 +0800

    fix(test): rollback merge style config issue

--- frontend/src/app/utils/chartHelper.ts
+++ frontend/src/app/utils/chartHelper.ts
@@ -631,7 +631,8 @@ export function mergeChartStyleConfigs(
       tEle['value'] = tEle?.['default'];
     }
 
-    const sEle = source?.find(s => s?.key === tEle.key) || source?.[index];
+    const sEle =
+      'key' in tEle ? source?.find(s => s?.key === tEle.key) : source?.[index];
 
     if (!isUndefined(sEle?.['value'])) {
       tEle['value'] = sEle?.['value'];
