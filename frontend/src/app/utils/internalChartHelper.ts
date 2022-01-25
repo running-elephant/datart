28ab390a9c31dd065c5a3810301d8d91ad7ab785
tianlei <qxqzx13@126.com>
2022-01-25 18:16:41 +0800

    fix:(Chart)fix filterSqlOperatorName function

--- frontend/src/app/utils/internalChartHelper.ts
+++ frontend/src/app/utils/internalChartHelper.ts
@@ -284,16 +284,16 @@ const balanceAssignConfigRows = sources => {};
 
 // 兼容 impala 聚合函数小写问题
 export const filterSqlOperatorName = (requestParams, widgetData) => {
-  const sqlOperatorNameList = requestParams.aggregators.map(({ sqlOperator }) =>
-    sqlOperator.toLocaleLowerCase(),
+  const sqlOperatorNameList = requestParams.aggregators.map(aggConfig =>
+    aggConfig.sqlOperator?.toLocaleLowerCase(),
   );
-  if (!sqlOperatorNameList.length) return widgetData;
+  if (!sqlOperatorNameList?.length) return widgetData;
   widgetData?.columns?.forEach(item => {
     const index = item.name.indexOf('(');
     const sqlOperatorName = item.name.slice(0, index);
     sqlOperatorNameList.includes(sqlOperatorName) &&
-      (item.name =
-        sqlOperatorName.toLocaleUpperCase() + item.name.slice(index));
+    (item.name =
+      sqlOperatorName.toLocaleUpperCase() + item.name.slice(index));
   });
   return widgetData;
 };
