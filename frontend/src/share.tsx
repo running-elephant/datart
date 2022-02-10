50e8758736e7e68e8891001eab2bafa7662a9dcf
scottsut <scottsut@163.com>
2022-02-10 08:16:28 +0800

    fix: change import order of antd.less

--- frontend/src/share.tsx
+++ frontend/src/share.tsx
@@ -1,4 +1,22 @@
-import 'antd/dist/antd.less';
+/**
+ * Datart
+ *
+ * Copyright 2021
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* eslint-disable prettier/prettier */
 import 'app/assets/fonts/iconfont.css';
 import { Share } from 'app/share';
 import React from 'react';
@@ -11,6 +29,7 @@ import { Provider } from 'react-redux';
 import { configureAppStore } from 'redux/configureStore';
 import { ThemeProvider } from 'styles/theme/ThemeProvider';
 import './locales/i18n';
+import 'antd/dist/antd.less'; // Must be at the end, override styles
 
 export const store = configureAppStore();
 const MOUNT_NODE = document.getElementById('root') as HTMLElement;
