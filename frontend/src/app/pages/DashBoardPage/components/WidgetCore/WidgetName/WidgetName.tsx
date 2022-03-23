234d76461ef79f31a42321ebf48d940c8805b74d
xieliuduo <xliuduo@163.com>
2022-03-23 19:48:15 +0800

    refactor: WidgetName

--- frontend/src/app/pages/DashBoardPage/components/WidgetCore/WidgetName/WidgetName.tsx
+++ frontend/src/app/pages/DashBoardPage/components/WidgetCore/WidgetName/WidgetName.tsx
@@ -16,7 +16,7 @@
  * limitations under the License.
  */
 
-import React, { FC } from 'react';
+import React, { FC, memo } from 'react';
 import styled from 'styled-components/macro';
 import {
   WidgetConf,
@@ -36,7 +36,9 @@ export const LabelName: FC<{ config: WidgetConf }> = ({ config }) => {
 export const WidgetName: FC<{
   config: WidgetConf;
   zIndex?: number;
-}> = ({ config, zIndex }) => {
+}> = memo(({ config, zIndex }) => {
+  console.log('- WidgetName');
+
   if (HideTitleTypes.includes(config.type)) {
     return null;
   }
@@ -49,19 +51,19 @@ export const WidgetName: FC<{
       <LabelName config={config} />
     </StyledWrap>
   );
-};
+});
 
 const StyledWrap = styled.div<{ conf: WidgetNameConfig; zIndex?: number }>`
   width: 100%;
   line-height: 24px;
-  cursor: pointer;
   text-align: ${p => p.conf.textAlign};
+  cursor: pointer;
 `;
 const NameWrap = styled.span<{ conf: WidgetNameConfig }>`
+  font-family: ${p => p.conf.fontFamily};
   font-size: ${p => p.conf.fontSize}px;
+  font-style: ${p => p.conf.fontStyle};
+  font-weight: ${p => p.conf.fontWeight};
   color: ${p => p.conf.color};
   text-align: ${p => p.conf.textAlign};
-  font-family: ${p => p.conf.fontFamily};
-  font-weight: ${p => p.conf.fontWeight};
-  font-style: ${p => p.conf.fontStyle};
 `;
