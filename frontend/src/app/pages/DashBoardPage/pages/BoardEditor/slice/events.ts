e5f0463ef0aeb7b283fd31959685aa721d9a69a9
xieliuduo <xliuduo@163.com>
2022-03-25 16:16:41 +0800

    refactor: widgetMoveEnd

--- frontend/src/app/pages/DashBoardPage/pages/BoardEditor/slice/events.ts
+++ frontend/src/app/pages/DashBoardPage/pages/BoardEditor/slice/events.ts
@@ -1,3 +1,30 @@
 import EventEmitter from 'events';
+const eventBus = new EventEmitter();
+const WIDGET_MOVE = 'widgetMove';
+const WIDGET_MOVE_END = 'widgetMoveEnd';
+interface FnWidgetMove {
+  (selectedIds: string[], deltaX: number, deltaY: number): void;
+}
 
-export const eventBus = new EventEmitter();
+export const widgetMove = {
+  on: (fn: FnWidgetMove) => {
+    eventBus.addListener(WIDGET_MOVE, fn);
+  },
+  emit: (selectedIds: string[], deltaX: number, deltaY: number) => {
+    eventBus.emit(WIDGET_MOVE, selectedIds, deltaX, deltaY);
+  },
+  off: (fn: FnWidgetMove) => {
+    eventBus.removeListener(WIDGET_MOVE, fn);
+  },
+};
+export const widgetMoveEnd = {
+  on: (fn: () => void) => {
+    eventBus.addListener(WIDGET_MOVE_END, fn);
+  },
+  emit: () => {
+    eventBus.emit(WIDGET_MOVE_END);
+  },
+  off: (fn: () => void) => {
+    eventBus.removeListener(WIDGET_MOVE_END, fn);
+  },
+};
