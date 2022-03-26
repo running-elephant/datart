6e2e93312b43d746d642bbdeba70511759fde771
xieliuduo <xliuduo@163.com>
2022-03-26 11:55:56 +0800

    refactor: UndoBtn RedoBtn

--- frontend/src/app/pages/DashBoardPage/pages/BoardEditor/components/BoardToolBar/UndoRedo/UndoRedo.tsx
+++ frontend/src/app/pages/DashBoardPage/pages/BoardEditor/components/BoardToolBar/UndoRedo/UndoRedo.tsx
@@ -19,20 +19,17 @@ import { RedoOutlined, UndoOutlined } from '@ant-design/icons';
 import { Tooltip } from 'antd';
 import { ToolbarButton } from 'app/components';
 import useI18NPrefix from 'app/hooks/useI18NPrefix';
-import { BOARD_UNDO } from 'app/pages/DashBoardPage/constants';
-import React, { useCallback, useEffect, useMemo } from 'react';
+import React, { useContext, useEffect, useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { ActionCreators } from 'redux-undo';
+import { BoardActionContext } from '../../../../../components/BoardProvider/BoardActionProvider';
 import { selectFutureState, selectPastState } from '../../../slice/selectors';
 
 export const UndoBtn = () => {
   const t = useI18NPrefix(`viz.board.action`);
-
+  const { undo } = useContext(BoardActionContext);
   const pastState = useSelector(selectPastState);
   const dispatch = useDispatch();
-  const Undo = useCallback(() => {
-    dispatch({ type: BOARD_UNDO.undo });
-  }, [dispatch]);
   const canUndo = useMemo(() => !!pastState.length, [pastState.length]);
   useEffect(() => {
     if (pastState.length === 1) {
@@ -45,7 +42,7 @@ export const UndoBtn = () => {
     <Tooltip title={t('undo')}>
       <ToolbarButton
         disabled={!canUndo}
-        onClick={Undo}
+        onClick={undo}
         icon={<UndoOutlined />}
       />
     </Tooltip>
@@ -54,16 +51,13 @@ export const UndoBtn = () => {
 export const RedoBtn = () => {
   const t = useI18NPrefix(`viz.board.action`);
   const futureState = useSelector(selectFutureState);
-  const dispatch = useDispatch();
-  const Redo = useCallback(() => {
-    dispatch({ type: BOARD_UNDO.redo });
-  }, [dispatch]);
+  const { redo } = useContext(BoardActionContext);
   const canRedo = useMemo(() => !!futureState.length, [futureState.length]);
   return (
     <Tooltip title={t('redo')}>
       <ToolbarButton
         disabled={!canRedo}
-        onClick={Redo}
+        onClick={redo}
         icon={<RedoOutlined />}
       />
     </Tooltip>
