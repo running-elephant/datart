cbacd6f066c8e1042a47acd9cac8bfe16f93a5c5
tianlei <qxqzx13@126.com>
2022-03-29 19:20:54 +0800

    feat:(Chart)add scorecard types

--- frontend/src/app/components/ChartGraph/Scorecard/ScorecardAdapter.tsx
+++ frontend/src/app/components/ChartGraph/Scorecard/ScorecardAdapter.tsx
@@ -18,63 +18,29 @@
 
 import { FC, memo } from 'react';
 import styled from 'styled-components/macro';
+import { AggregateBoxProp, ScorecardBoxProp, ScorecardConfig } from './types';
 
-interface FontConfig {
-  color: string;
-  fontFamily: string;
-  fontSize: string;
-  fontStyle: string;
-  fontWeight: string;
-  lineHeight: number;
-}
-
-const ScorecardAdapter: FC<{
-  dataConfig: FontConfig;
-  labelConfig: {
-    show: boolean;
-    font: FontConfig;
-    position: string;
-    alignment: string;
-  };
-  padding: string;
-  context: {
-    width: number;
-    height: number;
-  };
-  width: number;
-  data: {
-    label: string;
-    value: number | string;
-  };
-  background: string;
-}> = memo(({ dataConfig, labelConfig, padding, data, background }) => {
-  const ssp = e => {
-    e.stopPropagation();
-  };
-  return (
-    <ScorecardBox padding={padding} onClick={ssp} style={{ background }}>
-      <AggregateBox
-        alignment={labelConfig?.alignment || 'center'}
-        position={labelConfig?.position || 'column'}
-      >
-        <ValueBox style={dataConfig?.[0].font}>{data?.[0]?.value}</ValueBox>
-        {labelConfig?.show && (
-          <LabelBox style={labelConfig?.font}>{data?.[0]?.label}</LabelBox>
-        )}
-      </AggregateBox>
-    </ScorecardBox>
-  );
-});
+const ScorecardAdapter: FC<ScorecardConfig> = memo(
+  ({ dataConfig, labelConfig, padding, data, background }) => {
+    const ssp = e => {
+      e.stopPropagation();
+    };
+    return (
+      <ScorecardBox padding={padding} onClick={ssp} style={{ background }}>
+        <AggregateBox
+          alignment={labelConfig?.alignment || 'center'}
+          position={labelConfig?.position || 'column'}
+        >
+          <ValueBox style={dataConfig?.[0].font}>{data?.[0]?.value}</ValueBox>
+          {labelConfig?.show && (
+            <LabelBox style={labelConfig?.font}>{data?.[0]?.label}</LabelBox>
+          )}
+        </AggregateBox>
+      </ScorecardBox>
+    );
+  },
+);
 export default ScorecardAdapter;
-
-interface ScorecardBoxProp {
-  padding: string | number;
-}
-
-interface AggregateBoxProp {
-  position: string;
-  alignment: string;
-}
 
 const ScorecardBox = styled.div<ScorecardBoxProp>`
   display: flex;
