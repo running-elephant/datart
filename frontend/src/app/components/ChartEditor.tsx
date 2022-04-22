aa9ba3e7c2e73cd5a3192c3f07d9053985e7d35e
lyp000119 <2319460338@qq.com>
2022-04-22 17:10:55 +0800

    fix: Date Level supports all charts The bug that the switch date level button is not displayed when switching to default date Clear all temporary data when switching charts

--- frontend/src/app/components/ChartEditor.tsx
+++ frontend/src/app/components/ChartEditor.tsx
@@ -58,6 +58,7 @@ import { IChart } from 'app/types/Chart';
 import { IChartDrillOption } from 'app/types/ChartDrillOption';
 import { ChartDTO } from 'app/types/ChartDTO';
 import {
+  clearRuntimeDateLevelFieldsInChartConfig,
   getRuntimeComputedFields,
   getRuntimeDateLevelFields,
 } from 'app/utils/chartHelper';
@@ -224,6 +225,37 @@ export const ChartEditor: FC<ChartEditorProps> = ({
     }
   }, [dataview?.sourceId, dispatch]);
 
+  const handleDateAggregated = useCallback(
+    config => {
+      const index = config?.datas?.findIndex(
+        v => v.type === ChartDataSectionType.GROUP,
+      );
+      if (index !== undefined) {
+        const groupRows = config?.datas?.[index]?.rows;
+        if (groupRows) {
+          const dateLevelComputedFields = groupRows.filter(
+            v =>
+              v.category === ChartDataViewFieldCategory.DateLevelComputedField,
+          );
+
+          const computedFields = getRuntimeComputedFields(
+            dateLevelComputedFields,
+            '',
+            dataview?.computedFields,
+            chartConfig,
+          );
+
+          dispatch(
+            workbenchSlice.actions.updateCurrentDataViewComputedFields(
+              computedFields,
+            ),
+          );
+        }
+      }
+    },
+    [chartConfig, dataview?.computedFields, dispatch],
+  );
+
   const registerChartEvents = useCallback(
     chart => {
       chart?.registerMouseEvents([
@@ -303,10 +335,12 @@ export const ChartEditor: FC<ChartEditorProps> = ({
     setChart(c);
     const targetChartConfig = CloneValueDeep(c.config);
 
-    const finalChartConfig = transferChartConfigs(
-      targetChartConfig,
-      shadowChartConfig || chartConfig,
+    const finalChartConfig = clearRuntimeDateLevelFieldsInChartConfig(
+      transferChartConfigs(targetChartConfig, shadowChartConfig || chartConfig),
     );
+
+    handleDateAggregated(finalChartConfig);
+
     dispatch(
       workbenchSlice.actions.updateChartConfig({
         type: ChartConfigReducerActionType.INIT,
@@ -343,11 +377,12 @@ export const ChartEditor: FC<ChartEditorProps> = ({
         const dateLevelComputedFields = payload.value.rows.filter(
           v => v.category === ChartDataViewFieldCategory.DateLevelComputedField,
         );
+
         const replacedColName = payload.value.replacedColName;
         const computedFields = getRuntimeComputedFields(
           dateLevelComputedFields,
           replacedColName,
-          CloneValueDeep(dataview?.computedFields),
+          dataview?.computedFields,
           chartConfig,
         );
 
@@ -357,11 +392,16 @@ export const ChartEditor: FC<ChartEditorProps> = ({
           });
         }
 
-        dispatch(
-          workbenchSlice.actions.updateCurrentDataViewComputedFields(
-            computedFields,
-          ),
-        );
+        if (
+          JSON.stringify(computedFields) !==
+          JSON.stringify(dataview?.computedFields)
+        ) {
+          dispatch(
+            workbenchSlice.actions.updateCurrentDataViewComputedFields(
+              computedFields,
+            ),
+          );
+        }
       }
 
       dispatch(
@@ -424,6 +464,7 @@ export const ChartEditor: FC<ChartEditorProps> = ({
   }, [buildDataChart, chartType, dataview, onSaveInWidget]);
 
   const saveChart = useCallback(async () => {
+    handleDateAggregated(chartConfig);
     if (container === 'dataChart') {
       if (dataChartId) {
         await dispatch(
@@ -507,6 +548,8 @@ export const ChartEditor: FC<ChartEditorProps> = ({
     chartConfig,
     dataview?.computedFields,
     history,
+    chartConfig,
+    handleDateAggregated,
   ]);
 
   const saveChartToDashBoard = useCallback(
