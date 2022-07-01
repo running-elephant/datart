fabbedbb7827f09bbafa696b3d06c4d700e9f89f
Wang Shu Guang <wang.shuguang@siemens.com>
2022-07-01 15:21:31 +0800

    fix: upgrade to spring-boot 2.7.1, affected swagger, flyway, bean cycle

--- server/src/main/java/datart/server/config/SwaggerConfiguration.java
+++ server/src/main/java/datart/server/config/SwaggerConfiguration.java
@@ -19,36 +19,34 @@
 
 package datart.server.config;
 
-import com.fasterxml.classmate.ResolvedType;
 import com.google.common.collect.Lists;
-import datart.server.base.dto.ResponseData;
-import org.apache.commons.lang3.reflect.TypeUtils;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.schema.ResolvedTypes;
-import springfox.documentation.schema.TypeNameExtractor;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
-
-import springfox.documentation.spi.schema.contexts.ModelContext;
-import springfox.documentation.spi.service.OperationBuilderPlugin;
-import springfox.documentation.spi.service.contexts.OperationContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.spring.web.plugins.WebFluxRequestHandlerProvider;
+import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
 
-import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Configuration
-@EnableSwagger2
+@EnableOpenApi
 public class SwaggerConfiguration {
 
 //    @Autowired
@@ -59,19 +57,26 @@ public class SwaggerConfiguration {
 
     @Bean
     public Docket createRestApi() {
-
-        List<ResponseMessage> responseMessageList = new ArrayList<>();
-        return new Docket(DocumentationType.SWAGGER_2)
-                .globalResponseMessage(RequestMethod.GET, responseMessageList)
-                .globalResponseMessage(RequestMethod.POST, responseMessageList)
-                .globalResponseMessage(RequestMethod.PUT, responseMessageList)
-                .globalResponseMessage(RequestMethod.DELETE, responseMessageList)
-                .apiInfo(apiInfo()).ignoredParameterTypes(Exception.class)
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .groupName("SwaggerGroupOneAPI")
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("datart"))
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
-                .build()
-                .securitySchemes(Lists.newArrayList(apiKey()));
+                .build();
+
+//        List<ResponseMessage> responseMessageList = new ArrayList<>();
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .globalResponseMessage(RequestMethod.GET, responseMessageList)
+//                .globalResponseMessage(RequestMethod.POST, responseMessageList)
+//                .globalResponseMessage(RequestMethod.PUT, responseMessageList)
+//                .globalResponseMessage(RequestMethod.DELETE, responseMessageList)
+//                .apiInfo(apiInfo()).ignoredParameterTypes(Exception.class)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("datart"))
+//                .paths(PathSelectors.any())
+//                .build()
+//                .securitySchemes(Lists.newArrayList(apiKey()));
     }
 
 
@@ -105,6 +110,40 @@ public class SwaggerConfiguration {
 
     private ApiKey apiKey() {
         return new ApiKey("Authorization", "Authorization", "header");
+    }
+
+
+    @Bean
+    public static BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
+        return new BeanPostProcessor() {
+
+            @Override
+            public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+                if (bean instanceof WebMvcRequestHandlerProvider || bean instanceof WebFluxRequestHandlerProvider) {
+                    customizeSpringfoxHandlerMappings(getHandlerMappings(bean));
+                }
+                return bean;
+            }
+
+            private <T extends RequestMappingInfoHandlerMapping> void customizeSpringfoxHandlerMappings(List<T> mappings) {
+                List<T> copy = mappings.stream()
+                        .filter(mapping -> mapping.getPatternParser() == null)
+                        .collect(Collectors.toList());
+                mappings.clear();
+                mappings.addAll(copy);
+            }
+
+            @SuppressWarnings("unchecked")
+            private List<RequestMappingInfoHandlerMapping> getHandlerMappings(Object bean) {
+                try {
+                    Field field = ReflectionUtils.findField(bean.getClass(), "handlerMappings");
+                    field.setAccessible(true);
+                    return (List<RequestMappingInfoHandlerMapping>) field.get(bean);
+                } catch (IllegalArgumentException | IllegalAccessException e) {
+                    throw new IllegalStateException(e);
+                }
+            }
+        };
     }
 
 }
