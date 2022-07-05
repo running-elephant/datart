0654b59cd61ed1d3df6527da7864785b02da0cc2
Stephen Cui <229063661@qq.com>
2022-07-05 17:43:41 +0800

    refactor: remove font smoothing

--- frontend/src/styles/globalStyles/overwritten/hardcoded.ts
+++ frontend/src/styles/globalStyles/overwritten/hardcoded.ts
@@ -21,8 +21,6 @@ import { FONT_SIZE_LABEL, SPACE_SM } from 'styles/StyleConstants';
 
 export const Hardcoded = createGlobalStyle`
   body {
-    -webkit-font-smoothing: antialiased;
-
     .ant-form-item-label > label {
       color: ${p => p.theme.textColorLight};
     }
