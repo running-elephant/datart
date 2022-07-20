30858f3f4a1f893690b6130315f01734233b37ff
lyp000119 <2319460338@qq.com>
2022-07-20 16:52:00 +0800

    feat: Save to Dashboard popup adds an entry for a create dashboard #1060

--- frontend/src/app/components/SaveToDashboard.tsx
+++ frontend/src/app/components/SaveToDashboard.tsx
@@ -16,7 +16,7 @@
  * limitations under the License.
  */
 
-import { Input, message, Modal } from 'antd';
+import { Button, Input, message, Modal, Space } from 'antd';
 import { useDebouncedSearch } from 'app/hooks/useDebouncedSearch';
 import useGetVizIcon from 'app/hooks/useGetVizIcon';
 import useI18NPrefix from 'app/hooks/useI18NPrefix';
@@ -26,13 +26,24 @@ import {
   PermissionLevels,
   ResourceTypes,
 } from 'app/pages/MainPage/pages/PermissionPage/constants';
+import { useAddViz } from 'app/pages/MainPage/pages/VizPage/hooks/useAddViz';
+import { SaveFormContext } from 'app/pages/MainPage/pages/VizPage/SaveFormContext';
 import { selectVizs } from 'app/pages/MainPage/pages/VizPage/slice/selectors';
 import { Folder } from 'app/pages/MainPage/pages/VizPage/slice/types';
 import {
   selectIsOrgOwner,
   selectPermissionMap,
 } from 'app/pages/MainPage/slice/selectors';
-import { FC, memo, useCallback, useEffect, useMemo, useState } from 'react';
+import { CommonFormTypes } from 'globalConstants';
+import {
+  FC,
+  memo,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
 import { useSelector } from 'react-redux';
 import styled from 'styled-components/macro';
 import { request2 } from 'utils/request';
@@ -61,9 +72,12 @@ const SaveToDashboard: FC<SaveToDashboardTypes> = memo(
     const [vizData, setVizData] = useState<Folder[]>(vizs);
     const [selectId, setSelectId] = useState<string>('');
     const t = useI18NPrefix('components.saveToDashOrStory');
+    const tgb = useI18NPrefix('global.button');
     const getIcon = useGetVizIcon();
     const isOwner = useSelector(selectIsOrgOwner);
     const permissionMap = useSelector(selectPermissionMap);
+    const { showSaveForm } = useContext(SaveFormContext);
+    const addVizFn = useAddViz({ showSaveForm });
 
     const selectDashboard = useCallback((dashboardData, event) => {
       setSelectId(event.node.relId);
@@ -131,15 +145,35 @@ const SaveToDashboard: FC<SaveToDashboardTypes> = memo(
         return d.name.toLowerCase().includes(keywords.toLowerCase());
       });
 
+    const createDashboard = useCallback(() => {
+      addVizFn({
+        vizType: 'DASHBOARD',
+        type: CommonFormTypes.Add,
+        visible: true,
+        initialValues: undefined,
+      });
+    }, [addVizFn]);
+
     return (
       <Modal
         title={title}
         visible={isModalVisible}
-        onOk={() => {
-          saveToDashboard(selectId);
-        }}
+        footer={
+          <Space>
+            <Button onClick={createDashboard}>{t('creatdDashboard')}</Button>
+            <Button onClick={handleCancel}>{tgb('cancel')}</Button>
+            <Button
+              onClick={() => {
+                saveToDashboard(selectId);
+              }}
+              disabled={!selectId}
+              type="primary"
+            >
+              {tgb('ok')}
+            </Button>
+          </Space>
+        }
         onCancel={handleCancel}
-        okButtonProps={{ disabled: !selectId }}
       >
         <InputWrap>
           <Input onChange={treeSearch} placeholder={t('searchValue')} />
