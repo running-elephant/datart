095129e32564f7443a3df3fdea0d639e21d335b2
tianlei <qxqzx13@126.com>
2022-08-17 12:59:35 +0800

    refactor(permission): refactor schedule permission

--- frontend/src/app/pages/MainPage/pages/SchedulePage/index.tsx
+++ frontend/src/app/pages/MainPage/pages/SchedulePage/index.tsx
@@ -1,22 +1,37 @@
+import useI18NPrefix from 'app/hooks/useI18NPrefix';
 import React from 'react';
 import { Route } from 'react-router-dom';
 import styled from 'styled-components/macro';
 import { useVizSlice } from '../VizPage/slice';
 import { EditorPage } from './EditorPage';
+import { SaveForm } from './SaveForm';
+import { SaveFormContext, useSaveFormContext } from './SaveFormContext';
 import { Sidebar } from './Sidebar';
 import { useScheduleSlice } from './slice';
 
 export function SchedulePage() {
+  const tg = useI18NPrefix('global');
+  const saveFormContextValue = useSaveFormContext();
   useScheduleSlice();
   useVizSlice();
   return (
-    <Container>
-      <Sidebar />
-      <Route
-        path="/organizations/:orgId/schedules/:scheduleId"
-        component={EditorPage}
-      />
-    </Container>
+    <SaveFormContext.Provider value={saveFormContextValue}>
+      <Container>
+        <Sidebar />
+        <Route
+          path="/organizations/:orgId/schedules/:scheduleId"
+          component={EditorPage}
+        />
+        <SaveForm
+          formProps={{
+            labelAlign: 'left',
+            labelCol: { offset: 1, span: 8 },
+            wrapperCol: { span: 13 },
+          }}
+          okText={tg('button.save')}
+        />
+      </Container>
+    </SaveFormContext.Provider>
   );
 }
 
