8e2690c1ae8c46aa1865d3c66d00251f76f9dd34
scottsut <scottsut@163.com>
2023-09-22 15:45:17 +0800

    chore: Update version to 1.0.0-rc.3

--- frontend/src/app/migration/constants.ts
+++ frontend/src/app/migration/constants.ts
@@ -27,6 +27,7 @@ export const APP_VERSION_BETA_4_2 = '1.0.0-beta.4+2';
 export const APP_VERSION_RC_0 = '1.0.0-RC.0';
 export const APP_VERSION_RC_1 = '1.0.0-RC.1';
 export const APP_VERSION_RC_2 = '1.0.0-RC.2';
+export const APP_VERSION_RC_3 = '1.0.0-RC.3';
 
 export const APP_SEMANTIC_VERSIONS = [
   APP_VERSION_INIT,
@@ -40,6 +41,7 @@ export const APP_SEMANTIC_VERSIONS = [
   APP_VERSION_RC_0,
   APP_VERSION_RC_1,
   APP_VERSION_RC_2,
+  APP_VERSION_RC_3,
 ];
 
 export const APP_CURRENT_VERSION =
